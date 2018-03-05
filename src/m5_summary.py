b86d746e89503568d8c7e84b32d3e09d2399f5b0
chenl4 <chenl4@rose-hulman.edu>
2018-03-05 09:50:13 -0500

    No meaning commit message

--- src/m5_summary.py
+++ src/m5_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Lilin Chen.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -27,3 +27,27 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ###############################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen = rg.Pen('red', 3)
+blue_turtle.speed = 20  # Fast
+
+size = 300
+
+for k in range(11):
+
+    blue_turtle.draw_square(size)
+
+    blue_turtle.pen_up()
+    blue_turtle.right(45)
+    blue_turtle.forward(20)
+    blue_turtle.left(45)
+
+    blue_turtle.pen_down()
+    size = size - 30
+
+window.close_on_mouse_click()
