18c94abaab50eac727a077235acd287926a215d7
chenl4 <chenl4@rose-hulman.edu>
2018-03-05 10:44:24 -0500

    No meaning commit message

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -31,8 +31,33 @@ import rosegraphics as rg
 
 window = rg.TurtleWindow()
 
-red_turtle = rg.SimpleTurtle('turtle')
-red_turtle.pen = rg.Pen('red',6)
-red_turtle.speed = 30
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen = rg.Pen('blue',6)
+blue_turtle.speed = 10
+
+green_turtle = rg.SimpleTurtle('turtle')
+green_turtle.pen = rg.Pen('green',6)
+green_turtle.speed = 9
 
 size = 300
+
+for k in range(11):
+
+    blue_turtle.draw_square(size)
+    green_turtle.draw_square(size)
+
+    blue_turtle.pen_up()
+    green_turtle.pen_up()
+    blue_turtle.right(45)
+    green_turtle.right(135)
+    blue_turtle.forward(20)
+    green_turtle.forward(20)
+    blue_turtle.left(45 + k)
+    green_turtle.left(45 + k)
+
+    blue_turtle.pen_down()
+    green_turtle.pen_down()
+    size = size - 30
+
+
+window.close_on_mouse_click()
